c31f790ffb4e503601657d35e521992db5cde0ac
Samanthags <samanthags3644@gmail.com>
2020-09-06 22:19:22 -0400

    navbar update

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,22 +7,24 @@ const STYLES = ['btn==primary' , 'btn--outline']
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => { const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => { 
+    
+const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
 
-const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
 return (
     <Link to='/sign=up' className='btn-mobile'>
         <button className={`btn= ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
-        type={type}
-        
+        type={type}     
         >
             
             {children}
         </button>
     </Link>
-)
+);
 
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 
 function Navbar() {
     const[click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick (!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>        
@@ -32,9 +45,10 @@ function Navbar() {
                         <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>Sign Up </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
-
+        
         </>
     
     )
